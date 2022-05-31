6e4ea984a3d8e4670d6c186a513eb05bd6626db7
MatiasSingermann <101400498+MatiasSingermann@users.noreply.github.com>
2022-05-31 14:30:45 -0300

    Movimiento del personaje

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public GameObject myBody;
+    public GameObject myCamera;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, speed);
+            myBody.transform.Translate(0, 0, speed);
+            myCamera.transform.Translate(0, 0, speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -speed);
+            myBody.transform.Translate(0, 0, -speed);
+            myCamera.transform.Translate(0, 0, -speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-speed, 0, 0);
+            myBody.transform.Translate(-speed, 0, 0);
+            myCamera.transform.Translate(-speed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(speed, 0, 0);
+            myBody.transform.Translate(speed, 0, 0);
+            myCamera.transform.Translate(speed, 0, 0);
+        }
+    }
+}
