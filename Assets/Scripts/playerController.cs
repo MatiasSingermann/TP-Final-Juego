b12a15a7ad34586ad4822bdf7fd4e8fe06bfea35
MatiasSingermann <101400498+MatiasSingermann@users.noreply.github.com>
2022-06-01 15:49:43 -0300

    Asset agregado

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -4,42 +4,53 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
-    public GameObject myBody;
-    public GameObject myCamera;
-    public float speed;
+    public float movementSpeed;
+
+
+    public float sensX;
+    public float sensY;
+
+    public Transform orientation;
+
+    float xRotation;
+    float yRotation;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.W))
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.eulerAngles += new Vector3(xRotation, yRotation, 0);
+        orientation.eulerAngles += new Vector3(0, yRotation, 0);
+
+        if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(0, 0, speed);
-            myBody.transform.Translate(0, 0, speed);
-            myCamera.transform.Translate(0, 0, speed);
+            transform.Translate(0, 0, movementSpeed);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(0, 0, -speed);
-            myBody.transform.Translate(0, 0, -speed);
-            myCamera.transform.Translate(0, 0, -speed);
+            transform.Translate(0, 0, -movementSpeed);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(-speed, 0, 0);
-            myBody.transform.Translate(-speed, 0, 0);
-            myCamera.transform.Translate(-speed, 0, 0);
+            transform.Translate(-movementSpeed, 0, 0);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(speed, 0, 0);
-            myBody.transform.Translate(speed, 0, 0);
-            myCamera.transform.Translate(speed, 0, 0);
+            transform.Translate(movementSpeed, 0, 0);
         }
     }
 }
