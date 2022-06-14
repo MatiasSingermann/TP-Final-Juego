3b369d09ee4c37d5ba5d09a7cf4a440276c02e8a
MatiasSingermann <101400498+MatiasSingermann@users.noreply.github.com>
2022-06-14 19:41:45 -0300

    Todas las plataformas funcionando

--- Assets/Scripts/playerShoot.cs
+++ Assets/Scripts/playerShoot.cs
@@ -22,6 +22,10 @@ public class playerShoot : MonoBehaviour
 
     public GameObject myPlatform;
 
+    public RaycastHit hit;
+
+    public bool isHitting;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -39,16 +43,17 @@ public class playerShoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        RaycastHit hit;
         string gameObjectShot;
 
-        if (Physics.Raycast(myCamera.transform.position, myCamera.transform.forward, out hit, 30f))
+        isHitting = Physics.Raycast(myCamera.transform.position, myCamera.transform.forward, out hit, 30f);
+
+        if (isHitting)
         {
             Vector3 shotPosition = hit.point;
             float shootDistance = hit.distance;
             gameObjectShot = hit.collider.gameObject.name;
 
-            Debug.Log(gameObjectShot + " - " + scriptPaintPlatform.numberMaterial);
+            //Debug.Log(gameObjectShot + " - " + scriptPaintPlatform.numberMaterial);
 
             if (hit.transform.gameObject.CompareTag("tagPlatform"))
             {
@@ -57,33 +62,6 @@ public class playerShoot : MonoBehaviour
                 if (Input.GetMouseButtonDown(0))
                 {
                     source.PlayOneShot(spraySound);
-
-                    Debug.Log(scriptPaintPlatform.numberMaterial);
-
-                    if(scriptPaintPlatform.numberMaterial == 1 && scriptPlayerSprayColor.selectedColor == 0) // si el spray es rojo
-                    {
-                        scriptPaintPlatform.GetComponent<Renderer>().material = scriptPaintPlatform.White;
-                        Debug.Log("Contacto Rojo");
-                        scriptPaintPlatform.numberMaterial = 10;
-                    }
-                    if (scriptPaintPlatform.numberMaterial == 2 && scriptPlayerSprayColor.selectedColor == 1) // si el spray es azul
-                    {
-                        scriptPaintPlatform.GetComponent<Renderer>().material = scriptPaintPlatform.White;
-                        Debug.Log("Contacto Azul");
-                        scriptPaintPlatform.numberMaterial = 10;
-                    }
-                    if (scriptPaintPlatform.numberMaterial == 3 && scriptPlayerSprayColor.selectedColor == 2) // si el spray es amarillo
-                    {
-                        scriptPaintPlatform.GetComponent<Renderer>().material = scriptPaintPlatform.White;
-                        Debug.Log("Contacto Amarillo");
-                        scriptPaintPlatform.numberMaterial = 10;
-                    }
-                    if (scriptPaintPlatform.numberMaterial == 4 && scriptPlayerSprayColor.selectedColor == 3) // si el spray es verde
-                    {
-                        scriptPaintPlatform.GetComponent<Renderer>().material = scriptPaintPlatform.White;
-                        Debug.Log("Contacto Verde");
-                        scriptPaintPlatform.numberMaterial = 10;
-                    }
                 }
             }
             else
